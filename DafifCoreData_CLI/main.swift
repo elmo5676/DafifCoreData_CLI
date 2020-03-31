2f4d13439618255065c0b6338eefd07e057d4d33
4bitCrew <matt@4bitcrew.com>
2020-03-31 15:07:50 -0700

    Initial Commit

---
+++ DafifCoreData_CLI/main.swift
@@ -0,0 +1,12 @@
+//
+// ********************** main *********************************
+// * Copyright © 4bitCrew, LLC - All Rights Reserved
+// * Created on 3/31/20, for DafifCoreData_CLI
+// * Matthew Elmore <matt@4bitCrew.com>
+// * Unauthorized copying of this file is strictly prohibited
+// ********************** main *********************************
+
+import Foundation
+
+print("Hello, World!")
+
